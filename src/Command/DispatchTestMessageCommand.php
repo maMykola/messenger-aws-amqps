40237fa4243156aa398c0f4a68f29583c26885af
Mykola Martynov <mykola.martynov@hotmail.com>
2021-07-11 20:40:40 +0300

    create command to dispatch test message to the async transport

---
+++ src/Command/DispatchTestMessageCommand.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace App\Command;
+
+use App\Message\TestMessage;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Messenger\MessageBusInterface;
+
+
+class DispatchTestMessageCommand extends Command
+{
+    protected static $defaultName = 'app:dispatch-test-message';
+
+    protected MessageBusInterface $bus;
+
+    public function __construct(MessageBusInterface $bus)
+    {
+        parent::__construct();
+
+        $this->bus = $bus;
+    }
+
+    protected function configure()
+    {
+        $this->setDescription('Dispatch TestMessage to async transport.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $this->bus->dispatch(new TestMessage());
+
+        $io->success('TestMessage successfully dispatched');
+
+        return 0;
+    }
+}
